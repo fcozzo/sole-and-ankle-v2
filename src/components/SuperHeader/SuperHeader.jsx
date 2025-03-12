9b3e9b0afa4cba301f9b81f8e642db82762494d3
Fred Cozzo <fred.cozzo@gmail.com>
2025-03-11 22:48:07 -0500

    exercise 1 - super header

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -22,14 +22,23 @@ const SuperHeader = () => {
   );
 };
 
+const MarketingMessage = styled.span`
+  color: ${COLORS.white};
+`;
+
 const Wrapper = styled.div`
+  display: flex;
+    align-items: center;
+    gap: 24px;
+    padding-inline: 32px;
+    height: ${40 / 16}rem;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
-`;
-
-const MarketingMessage = styled.span`
-  color: ${COLORS.white};
+    
+    ${MarketingMessage} {
+        margin-inline-end: auto;
+    }
 `;
 
 const HelpLink = styled.a`
