9aa2bdb136598ffb280251a59f01773d04162fdd
Fred Cozzo <fred.cozzo@gmail.com>
2025-03-23 22:14:03 -0500

    exercise 4a - shoe grid layout

--- src/components/ShoeCard/ShoeCard.jsx
+++ src/components/ShoeCard/ShoeCard.jsx
@@ -51,7 +51,8 @@ const ShoeCard = ({
 };
 
 const Link = styled.a`
-  text-decoration: none;
+    flex: 1 0 340px;
+    text-decoration: none;
   color: inherit;
 `;
 
@@ -61,7 +62,9 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
